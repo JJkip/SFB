e6dde8d03ca944128196e276de91064818818c13
jjkip <jjlangatjose@gmail.com>
2023-09-08 23:59:48 +0300

    Classes refresh

---
+++ Ls9Classes.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+class Employee {
+    var name = ""
+    var salary = 0
+    var role = " "
+    
+    func doWork() {
+        print("Hi, my name is \(name) and i'm doing work")
+        salary += 1
+    }
+}
+
+let a:Int = 10
+let b:String = "Ted"
+let c:Employee = Employee()
+
+c.name = "Tom"
+c.role = "Art Dicector"
+c.salary = 1200
+print(c.salary)
+
+var d = Employee()
+d.name = "Jane"
+d.role = "Manager"
+d.salary = 2000
+
+d.doWork()
